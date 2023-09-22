14fcb6133ca6032d8560bb9215664fdc5dfd0727
shibuya-n <a.duong2006@gmail.com>
2023-09-22 15:02:56 -0700

    test

---
+++ src/word.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class word {
+
+    String name;
+
+    public String result = "";
+    public word(String x) {
+
+        name = x;
+
+
+
+    }
+
+    public String scramble(){
+        String scramble = "";
+        String temp = "";
+
+        scramble += name.substring(0,1);
+        temp += name.substring(1, name.length()-1);
+        System.out.println(temp);
+
+        for (int i = 0; i < name.length(); i++){
+            int random = (int)(Math.random() * temp.length()-1) + 1;
+
+            if (temp.length() == 1) {
+                scramble += temp;
+                break;
+            }
+            else {
+                scramble += temp.charAt(random);
+                System.out.println(scramble);
+
+                temp = temp.substring(0, random) + temp.substring(random + 1);
+                System.out.println(temp);
+                System.out.println();
+            }
+
+        }
+        scramble += name.substring(name.length()-1);
+        return scramble;
+    }
+
+    public static String name(){
+        System.out.println("First name and last name? ");
+        return getUserInput();
+
+    }
+    public static String getUserInput() {
+        Scanner input = new Scanner(System.in);
+        String response = input.nextLine();
+
+        return response;
+    }
+
+
+}
